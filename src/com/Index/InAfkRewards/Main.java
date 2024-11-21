7b6416d2bd598342e401555204eeda344ad6df03
[ASYNC] Squeeze <142709255+Indexa2@users.noreply.github.com>
2024-11-21 14:32:40 -0600

    Update Main.java

---
+++ src/com/Index/InAfkRewards/Main.java
@@ -0,0 +1,25 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this; 
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new YourCustomPlaceholderExpansion().register();
+            getLogger().info("PlaceholderAPI hook registered!");
+        } else {
+            getLogger().warning("PlaceholderAPI not found! Placeholder functionality will be disabled.");
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null; 
+    }
+
+    public static Main getInstance() {
+        return instance; 
+    }
+}
